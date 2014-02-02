99cb46c2dd640f200a81446d2232f9649be10e9a
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-02 16:02:12 +0400

    order_by

--- common/views.py
+++ common/views.py
@@ -128,7 +128,7 @@ class TodayEventsView(ListView):
     active = 'today'
 
     def get_queryset(self):
-        return self.request.user.events.filter(event_date=datetime.date.today())
+        return self.request.user.events.filter(event_date=datetime.date.today()).order_by('event_date')
 
     def get_context_data(self, **kwargs):
         ctx = super(TodayEventsView, self).get_context_data(**kwargs)
@@ -140,21 +140,21 @@ class TomorrowEventsView(TodayEventsView):
 
     def get_queryset(self):
         tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-        return self.request.user.events.filter(event_date=tomorrow)
+        return self.request.user.events.filter(event_date=tomorrow).order_by('event_date')
 
 class WeekEventsView(TodayEventsView):
     active = 'week'
 
     def get_queryset(self):
         week = datetime.date.today() + datetime.timedelta(days=7)
-        return self.request.user.events.filter(event_date__lte=week)
+        return self.request.user.events.filter(event_date__lte=week).order_by('event_date')
 
 class MonthEventsView(TodayEventsView):
     active = 'month'
 
     def get_queryset(self):
         month = datetime.date.today() + datetime.timedelta(days=30)
-        return self.request.user.events.filter(event_date__lte=month)
+        return self.request.user.events.filter(event_date__lte=month).order_by('event_date')
 
 class FeedbackView(TemplateView):
     template_name = 'feedback.html'
@@ -179,5 +179,5 @@ class HomeView(ListView):
 
     def get_queryset(self):
         if self.request.user.is_authenticated():
-            return self.request.user.events.all()
+            return self.request.user.events..order_by('event_date')
         return Event.objects.none()
