85405d232492b7c969fd3c71654fc6372efafa05
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-04 12:23:20 +0400

    test celery del old tasks

--- eventradar/tasks.py
+++ eventradar/tasks.py
@@ -6,7 +6,6 @@ from common.utils import del_old_evens
 @periodic_task(ignore_result=True, run_every=crontab())
 def clean_old_events():
     del_old_evens()
-    print 'TEST CELERY'
 
 @task
 def clean_events():
