2b3768a00e1f7f2dfca7bc3912727422086aa0e7
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-19 18:13:52 +0400

    nodejs add in mysql

--- test_node/client.js
+++ test_node/client.js
@@ -66,7 +66,7 @@ fs.readFile('urls.txt', function(err, logData)
     console.log('Count: '+urls.length);
     urls = objectDeDup(urls);
     console.log('Count of uniq: '+urls.length);
-    urls = urls.slice(0, 200);
+    urls = urls.slice(0, 600);
     nums = urls.length;
 
     console.log('Count fo urls: ' + urls.length);
