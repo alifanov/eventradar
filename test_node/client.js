45c0f4db97203c067b3a1ce02755597128c4fb06
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-19 18:21:47 +0400

    nodejs add in mysql

--- test_node/client.js
+++ test_node/client.js
@@ -134,7 +134,14 @@ fs.readFile('urls.txt', function(err, logData)
                                     conn.count('common_event', doc, function(err, cnt){
                                         if(cnt == 0){
                                             conn.insert('common_event', doc, function(err, recordID){
-                                                console.log('Add record ' + recordID);
+                                                if (err)
+                                                {
+                                                    console.log('Error: '+err);
+                                                }
+                                                else
+                                                {
+                                                    console.log('Add record ' + recordID);
+                                                }
                                             });
                                         }
                                     });
