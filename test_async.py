3ce122d0e0d1b08a4cdb5cb91f1e51be36620dcc
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-21 13:59:48 +0400

    test async get_posts

--- test_async.py
+++ test_async.py
@@ -79,11 +79,11 @@ def cb(resp):
                 if pattern_today.match(text) and not event_date == post_date.today(): continue
                 if pattern_tomorrow.match(text) and not event_date == post_date.today() + datetime.timedelta(days=1): continue
                 if datetime.datetime.strptime(event_date, u'%Y-%m-%d').date() > datetime.date.today() + datetime.timedelta(days=-1):
-                    cursor.execute('insert ignore into common_event (owner_id, post_date, event_date,text,source,link) values ({}, {}, {}, {}, {}, {})'
+                    cursor.execute(u'insert ignore into common_event (owner_id, post_date, event_date,text,source,link) values ({}, {}, {}, {}, {}, {})'
                     .format(post['to_id'],
                         post_date.isoformat(),
                         event_date,
-                        text.decode('utf-8'),
+                        text,
                         post['to_id'],
                         link
                     ))
