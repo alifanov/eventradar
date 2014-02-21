8330b077d6a5e5f6506af12bdf425d7d4a1a75bb
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-21 06:42:50 +0400

    test async get_posts

--- test_async.py
+++ test_async.py
@@ -20,14 +20,13 @@ print len(urls)
 db.close()
 
 def cb(resp):
-    print 'cb'
     try:
         for p in resp.body['response'][1:]:
             print p['to_id']
 
     except KeyError:
-        pass
-#        print resp.raw_body
+#        pass
+        print resp.raw_body
 
 for url in urls[:10]:
     unirest.get(url, callback=cb)
