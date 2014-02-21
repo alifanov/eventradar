a3cbb5f57992a2612e37cc7a610e0b353b144196
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-21 14:27:54 +0400

    test async get_posts

--- test_async.py
+++ test_async.py
@@ -22,7 +22,7 @@ def get_date_from_string(date_str):
         now = datetime.datetime.now()
         if day.isdigit():
             day = int(day)
-            date = datetime.date(day = day, month=months[month.lower()], year=now.year)
+            date = datetime.date(day = day, month=months[month.lower()]-1, year=now.year)
     return date
 
 months = {
