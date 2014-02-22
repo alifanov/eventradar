d77b3b50c0fd3386f1e38e69da0170b2ed7511ca
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-22 14:47:50 +0400

    simple web service delete friends of friends

--- eventradar/tasks.py
+++ eventradar/tasks.py
@@ -1,16 +1,17 @@
-from celery.task import task, periodic_task
+from celery import app
+#from celery.task import task, periodic_task
 from celery.schedules import crontab
 from common.utils import del_old_evens, process_for_user, get_all_uids
 from django.contrib.auth.models import User
 
-@periodic_task(ignore_result=True, run_every=crontab(hour=0, minute=0))
+@app.periodic_task(ignore_result=True, run_every=crontab(hour=0, minute=0))
 def clean_old_events():
     del_old_evens()
 
-@periodic_task(ignore_result=True, run_every=crontab(hour="*/4"))
+@app.periodic_task(ignore_result=True, run_every=crontab(hour="*/4"))
 def get_posts():
     get_all_uids()
 
-@task
+@app.task
 def get_new_posts(user):
     process_for_user(user)
