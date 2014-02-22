99f329bdef790a042e07b26517dea0d7b72836ec
Lifanov Alexander <lifanov.a.v@gmail.com>
2014-02-22 14:04:01 +0400

    simple web service delete friends of friends

--- eventradar/tasks.py
+++ eventradar/tasks.py
@@ -7,14 +7,10 @@ from django.contrib.auth.models import User
 def clean_old_events():
     del_old_evens()
 
-@periodic_task(ignore_result=True, run_every=crontab(hour="*/4"))
-def get_posts():
-    get_all_uids()
+#@periodic_task(ignore_result=True, run_every=crontab(hour="*/4"))
+#def get_posts():
+#    get_all_uids()
 
 #@task
 #def get_new_posts(user):
 #    process_for_user(user)
-
-@task
-def clean_events():
-    del_old_evens()
